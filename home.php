93660322d300f8bd727f7e6e849431a16820f62a
hngoc2801 <qaz29036@gmail.com>
2023-04-22 16:58:44 +0700

    22/4

---
+++ home.php
@@ -0,0 +1,107 @@
+<div class="row">
+			<div id="carousel-id" class="carousel slide" data-ride="carousel">
+				<ol class="carousel-indicators">
+					<li data-target="#carousel-id" data-slide-to="0" class=""></li>
+					<li data-target="#carousel-id" data-slide-to="1" class=""></li>
+					<li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+				</ol>
+				<div class="carousel-inner">
+					<div class="item">
+						<center>
+						<img src="img/1.jpg" style="height: 500px; width: 100%;">
+						</center>
+						<div class="container">
+							<div class="carousel-caption">
+								<h1>New Boat Model</h1>
+								<p>Available Now at Low Price. Booked Now :)</p>
+								<p><a class="btn btn-lg btn-primary" href="#" role="button">Register today</a></p>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<img src="img/2.jpg" style="height: 500px; width: 100%;">
+						<div class="container">
+							<div class="carousel-caption">
+								<h1>#Summer</h1>
+								<p>Its More Fun in the Palompon</p>
+								<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+							</div>
+						</div>
+					</div>
+					<div class="item active">
+						<img src="img/3.jpg" style="height: 500px; width: 100%;">
+						<div class="container">
+							<div class="carousel-caption">
+								<h1>Kalangaman Island.</h1>
+								<p>Envisioning a self sustaining and ecologically balanced Palompon for quality of life.</p>
+								<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+				<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+			</div>
+		</div>
+
+		<br />
+		<div class="row container-fluid">
+			<div class="col-md-4">
+				<div class="panel panel-info">
+					<div class="panel-heading">
+						<h3 class="panel-title"><center>About Us</center></h3>
+					</div>
+					<div class="panel-body">
+						The Tourism Office is located at Rizal Street Palompon Leyte. We offer motorboats
+						to rent we also offer affordable lodging right in the town center.
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="panel panel-success">
+					<div class="panel-heading">
+						<h3 class="panel-title"><center>Contact Us</center></h3>
+					</div>
+					<div class="panel-body">
+						Cloefe Paloma - Revera
+						Municipal Tourism Officer
+						email : clee_revera@yahoo.com
+						mobile : +63 917 303 7267
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="panel panel-warning">
+					<div class="panel-heading">
+						<h3 class="panel-title"><center>Environmental Management Specialist</center></h3>
+					</div>
+					<div class="panel-body">
+						Christopher Montebon
+						Booking Service Deck Officer
+						Mobile: +63 917 303 7269
+					</div>
+				</div>
+			</div>
+		</div>
+ 		
+ 		<p>
+ 			<center>
+ 			
+ 			</center>
+ 		</p>
+		
+		<footer style="background-color: white;">
+			<center>
+				&copy; All rights reserved
+			</center>
+		</footer>
+
+ 		<script src="bootstrap/js/jquery-1.11.1.min.js"></script>
+ 		<script src="bootstrap/js/dataTables.js"></script>
+ 		<script src="bootstrap/js/dataTables2.js"></script>
+ 		<script src="bootstrap/js/bootstrap.js"></script>
+
+	</body>
+</html>
